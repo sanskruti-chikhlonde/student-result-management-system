d7488fdd634179a3b0fdbe42448094a0f51b4932
sanskruti-chikhlonde <sanskruti0804@gmail.com>
2026-08-04 15:31:04 +0530

    Select DOM elements for GradeBook

---
+++ script.js
@@ -0,0 +1,32 @@
+// student Information
+
+const studentName = document.getElementsByName ("studentName");
+const rollNumber = document.getElementsById ("rollNumber");
+
+//subject Name
+
+const subject1 = document.getElementById("subject1");
+const subject2 = document.getElementById("subject2");
+const subject3 = document.getElementById("subject3");
+const subject4 = document.getElementById("subject4");
+const subject5 = document.getElementById("subject5");   
+
+//subject Marks
+
+const marks1 = document.getElementById("marks1");   
+const marks2 = document.getElementById("marks2");
+const marks3 = document.getElementById("marks3");
+const marks4 = document.getElementById("marks4");
+const marks5 = document.getElementById("marks5");
+
+//button
+
+const calculateBtn = document.getElementById("calculateBtn");
+
+//result
+
+const totalMarks = document.getElementById("totalMarks");
+const average = document.getElementById("average");
+const percentage = document.getElementById("percentage");
+const grade = document.getElementById("grade");
+const status = document.getElementById("status");
